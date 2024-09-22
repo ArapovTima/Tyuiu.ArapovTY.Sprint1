4a0db085584915f37233b077b606cc5f333ef15d
Тимофей Арапов <Tyuiu.ArapovTY@outlook.com>
2024-09-22 19:14:18 +0500

    Task3

--- Tyuiu.ArapovTY.Sprint1.Task3.V9.Lib/DataService.cs
+++ Tyuiu.ArapovTY.Sprint1.Task3.V9.Lib/DataService.cs
@@ -1,7 +1,7 @@
-﻿//using tyuiu.cources.programming.interfaces.Sprint1;
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
 namespace Tyuiu.ArapovTY.Sprint1.Task3.V9.Lib
 {
-    public class DataService //: ISprint1Task3V9
+    public class DataService : ISprint1Task3V9
     {
         public double ConvertMinutesToHours(int minutes)
         {
