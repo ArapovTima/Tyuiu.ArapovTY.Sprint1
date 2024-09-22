02a9574662affdcb6f30363c3125ab4c34015574
Тимофей Арапов <Tyuiu.ArapovTY@outlook.com>
2024-09-22 19:55:25 +0500

    Task4

---
+++ Tyuiu.ArapovTY.Sprint1.Task4.V20.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.ArapovTY.Sprint1.Task4.V20.Lib;
+namespace Tyuiu.ArapovTY.Sprint1.Task4.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 5;
+            double y = 2;
+            double wait = 2;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
