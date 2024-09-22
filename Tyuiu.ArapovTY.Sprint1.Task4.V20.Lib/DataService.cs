885ccdfcd852fd6bdcd3c6d36df2cb68bc1c39ae
Тимофей Арапов <Tyuiu.ArapovTY@outlook.com>
2024-09-22 19:56:10 +0500

    Task4

--- Tyuiu.ArapovTY.Sprint1.Task4.V20.Lib/DataService.cs
+++ Tyuiu.ArapovTY.Sprint1.Task4.V20.Lib/DataService.cs
@@ -1,7 +1,7 @@
-﻿//using tyuiu.cources.programming.interfaces.Sprint1;
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
 namespace Tyuiu.ArapovTY.Sprint1.Task4.V20.Lib
 {
-    public class DataService //: ISprint1Task4V20
+    public class DataService : ISprint1Task4V20
     {
         public double Calculate(double x, double y)
         {
