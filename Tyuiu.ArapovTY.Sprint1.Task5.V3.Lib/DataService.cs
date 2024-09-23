3636363f35d355c911c853a5fea326b56e3c1974
Тимофей Арапов <Tyuiu.ArapovTY@outlook.com>
2024-09-23 19:29:57 +0500

    Task5

--- Tyuiu.ArapovTY.Sprint1.Task5.V3.Lib/DataService.cs
+++ Tyuiu.ArapovTY.Sprint1.Task5.V3.Lib/DataService.cs
@@ -1,7 +1,7 @@
-﻿//using tyuiu.cources.programming.interfaces.Sprint1;
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
 namespace Tyuiu.ArapovTY.Sprint1.Task5.V3.Lib
 {
-    public class DataService //: ISprint1Task5V3
+    public class DataService : ISprint1Task5V3
     {
         public int Calculate(int k)
         {
